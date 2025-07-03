9cc8af5b845d52badb6b75286fb284a139614fe9
cold <kholodihor@gmail.com>
2025-07-03 09:58:08 +0200

    feat: add fonts and minimal setup

---
+++ src/views/LandingView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Here will be Landing Page soon...</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  max-width: var(--container-max-width);
+  margin: 0 auto;
+  padding: 0 var(--spacing-unit);
+}
+
+h1 {
+  margin-top: 2rem;
+}
+</style>
